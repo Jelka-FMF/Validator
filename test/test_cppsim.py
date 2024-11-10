7412fb82b85ecaf420d0d3cb0c85f1b7023f8a37
Jošt Smrtnik <jost.smrtnik@gmail.com>
2024-11-10 20:28:42 +0100

    Tested on actual c++ pattern

--- test/test_cppsim.py
+++ test/test_cppsim.py
@@ -1,7 +1,6 @@
 from test.simulator import Simulation
 
 from subprocess import Popen, PIPE
-import sys
 import time
 from src.jelka_validator.datareader import DataReader
 
@@ -9,16 +8,16 @@ from src.jelka_validator.datareader import DataReader
 if __name__ == "__main__":
     # Popen(["-m", "writer.py"], executable=sys.executable, stdout=PIPE)
     # Popen(["writer.exe"], stdout=PIPE)
-    with Popen(["-m", "test/writer.py"], executable=sys.executable, stdout=PIPE, bufsize=10000) as p:
+    with Popen(["./test/writer"], stdout=PIPE, bufsize=10000) as p:
         sim = Simulation()
         dr = DataReader(p.stdout.read1)
         dr.update()
+        print(dr.header)
         # assert dr.header is not None
         sim.init()
         time.sleep(1)
         while sim.running:
             c = next(dr)
-            assert all(c[i] == c[0] for i in range(len(c)))
             dr.user_print()
             sim.set_colors(dict(zip(range(len(c)), c)))
             sim.frame()
--- test/writer.cpp
+++ test/writer.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
+void printhex(int x) {
+    if (x < 16) {
+        cout << "0";
+    }
+    cout << hex << x;
+}
+
 
 int main() {
-    cout << (char) 2 << (char) 0 << (char) 1 << (char) 244 << (char) 21 << (char) 24 <<
-    "Jošt Smrtnik" << (char) 0 << "Najboljši vzorec" << (char) 0 << (char) 3 << flush;
+    cout << "#{\"version\": 0, \"led_count\": 500, \"duration\": 10800, \"fps\": 60, \"author\": \"Jo\\u0161t\", \"title\": \"\\u010d\\u0161\\u0111\\u010d\\u010dsmf?=!9\\\"'\", \"school\": \"O\\u0160 .-,\"}\n" << endl;
 
-    for (int i = 0; i < 5400; i++) {
-        cout << (char) 2;
-        for (int j = 0; j < 500; j++) {
-            for (int k = 0; k < 3; k++) {
-                char p = i;
-                if (p == 2) {
-                    p = 1;
-                } else if (p == 3) {
-                    p = 4;
-                } else if (p == 10) {
-                    p = 9;
-                } else if (p == 13) {
-                    p = 14;
-                }
-                cout << (char) p;
+    for (int frame = 0; frame < 10800; frame++) {
+        cout << "#";
+        for (int led = 0; led < 500; led++) {
+            for (int rgb = 0; rgb < 3; rgb++) {
+                printhex((led + frame + rgb * 50) % 256);
             }
         }
-        
-        cout << (char) 3 << flush;
+        cout << endl;
+        if (frame % 100 == 0) {
+            cerr << "Frame " << frame << " / 10800" << endl;
+        }
     }
     
 }
